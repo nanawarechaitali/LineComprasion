993c7235fb654081a5040a4e458c5396132704cc
Your Name <nanawarechaitali4196@gmail.com>
2023-01-31 16:22:38 +0530

    first commit

---
+++ src/com/bridgelabz/LineComprasion.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class LineComprasion {
+    public static void main(String[] args) {
+        System.out.println("Welcome to line Comprasion Computation Program on Master branch");
+    }
+}
